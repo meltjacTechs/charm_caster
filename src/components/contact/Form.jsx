618eb319d7f21dae3c1c098db72fff34f92b1507
meltjacTechs <ebwire02@gmail.com>
2024-03-17 16:25:11 +0300

    Update recipient name in contact form

--- src/components/contact/Form.jsx
+++ src/components/contact/Form.jsx
@@ -75,7 +75,7 @@ export default function Form() {
 
   const onSubmit = (data) => {
     const templateParams = {
-      to_name: "CodeBucks",
+      to_name: "Order of the Mithrandir Forge",
       from_name: data.name,
       reply_to: data.email,
       message: data.message,
